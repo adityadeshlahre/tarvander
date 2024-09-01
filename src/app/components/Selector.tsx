6e4d31fc6cb123979b0578cb6e88d69413cf3e29
Aditya @ArchLinux <132184385+adityadeshlahre@users.noreply.github.com>
2024-09-01 16:12:58 +0530

    ui(responsiveness): minnor fix

--- src/app/components/Selector.tsx
+++ src/app/components/Selector.tsx
@@ -25,12 +25,14 @@ export default function Selector({
 }: SelectorProps) {
   return (
     <>
-      <div className="flex flex-col md:flex-row items-center m-2 p-4 space-y-4 md:space-y-0 md:space-x-4">
-        <div className="flex flex-col items-center text-slate-800 rounded-lg shadow-lg p-4 w-full md:w-3/5 space-y-4">
-          <div className="flex flex-col justify-center items-center space-y-4 md:space-y-0 md:space-x-4 w-full">
-            <div className="flex flex-col w-full md:w-full">
-              <div className="flex flex-row w-full space-y-2">
-                <div className="mb-2 font-bold">Start Point:</div>
+      <div className="flex flex-col md:flex-row items-center m-2 p-4 space-y-4 md:space-y-0 md:space-x-4 w-full">
+        <div className="flex flex-col items-center text-slate-800 rounded-lg shadow-lg p-4 w-full space-y-4">
+          <div className="flex flex-col md:flex-row justify-center items-start md:items-center space-y-4 md:space-y-0 md:space-x-4 w-full min-w-full">
+            <div className="flex flex-col w-full md:w-1/2">
+              <div className="flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-2">
+                <label htmlFor="start-point" className="mb-2 font-bold">
+                  Start Point:
+                </label>
                 <select
                   className="border rounded p-2 bg-inherit"
                   value={selectedStarting.name}
@@ -43,16 +45,18 @@ export default function Selector({
                   ))}
                 </select>
               </div>
-              <div className="m-2">
+              <div className="flex justify-center m-2">
                 <div>
                   Date & Time:{" "}
                   {new Date(selectedStarting.dateTime).toLocaleString()}
                 </div>
               </div>
             </div>
-            <div className="flex flex-col w-full md:w-full">
-              <div className="flex flex-row w-full space-y-2">
-                <div className="mb-2 font-bold">End Point:</div>
+            <div className="flex flex-col w-full md:w-1/2">
+              <div className="flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-2">
+                <label htmlFor="end-point" className="mb-2 font-bold">
+                  End Point:
+                </label>
                 <select
                   className="border rounded p-2 bg-inherit"
                   value={selectedEnding.name}
@@ -65,7 +69,7 @@ export default function Selector({
                   ))}
                 </select>
               </div>
-              <div className="m-2">
+              <div className="flex justify-center m-2">
                 <div>
                   Date & Time:{" "}
                   {new Date(selectedEnding.dateTime).toLocaleString()}
